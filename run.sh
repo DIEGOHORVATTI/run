ed11cf627ee5757c03bd9d8dee558355752a0136
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-02 16:42:56 -0400

    fix: case crete file

--- run.sh
+++ run.sh
@@ -3,54 +3,69 @@
 #                      automação de copilador em shellscript                         #
 ######################################################################################
 
+function versao_f(){
+	echo -e "\n"
+	echo -e "┌───────────────────────────────────────┐"
+	echo -e "│                                       │"
+	echo -e "│    Run v0.0.1 | by Diego Horvatti     │"
+	echo -e "│   https://github.com/DIEGOHORVATTI    │"
+	echo -e "│                                       │"
+	echo -e "└───────────────────────────────────────┘"
+	echo -e "\n"
+}
+
+function help_f(){
+	echo -e "\n"
+	echo -e "┌───────────────────────────────────────┐"
+	echo -e "│                                       │"
+	echo -e "│ -v, --version = versão do soft        │"
+	echo -e "│ -h, --help = informações das flags    │"
+	echo -e "│ -c, --create-file = criar arquivo.c   │"
+	echo -e "│ -g, --gitgnore = criar .gitgnore      │"
+	echo -e "│                                       │"
+	echo -e "│ Dependencias: 'colorls', 'grep'       │"
+	echo -e "│ 'cppcheck', 'wget'                    │"
+	echo -e "│                                       │"
+	echo -e "│ Como usar ex: run arquivo.c           │"
+	echo -e "│                                       │"
+	echo -e "└───────────────────────────────────────┘"
+	echo -e "\n"
+}
+
+function gitgnore_f(){
+	wget https://gist.githubusercontent.com/DIEGOHORVATTI/30462a7868ae1df718a6c32c505a9ea3/raw/77d407af7e041727e6025f30531683aaab893a7d/.gitignore
+	clear
+	echo -e "\n"
+	echo -e "┌───────────────────────────────────────┐"
+	echo -e "│                                       │"
+	echo -e "│        Create file:  .gitgnore       │"
+	echo -e "│                                       │"
+	echo -e "└───────────────────────────────────────┘"
+	echo -e "\n"
+}
+
+function creat_file_f(){
+	read -p ' New file: ' nome
+	echo -e "\n"
+	wget https://gist.githubusercontent.com/DIEGOHORVATTI/f1272263f3db0118f2d5d5bb3df0de8d/raw/71a598a9e538f4986527a10632db485dc36aed7c/min.c -O $nome.c
+	code $nome.c
+	clear
+}
+
 for i in "$@"; do
 	case $i in
+	"" | -v | --version) 
 		# ver versão, caso o parametro seja vazil ou -v 
-		"" | -v | --version) 
-			echo -e "\n"
-			echo -e "┌───────────────────────────────────────┐"
-			echo -e "│                                       │"
-			echo -e "│    Run v0.0.1 | by Diego Horvatti     │"
-			echo -e "│   https://github.com/DIEGOHORVATTI    │"
-			echo -e "│                                       │"
-			echo -e "└───────────────────────────────────────┘"
-			echo -e "\n"
+		versao_f
 		;;
 		-h | --help) 
-			echo -e "\n"
-			echo -e "┌───────────────────────────────────────┐"
-			echo -e "│                                       │"
-			echo -e "│ -v, --version = versão do soft        │"
-			echo -e "│ -h, --help = informações das flags    │"
-			echo -e "│ -c, --create-file = criar arquivo.c   │"
-			echo -e "│ -g, --gitgnore = criar .gitgnore      │"
-			echo -e "│                                       │"
-			echo -e "│ Dependencias: 'colorls', 'grep'       │"
-			echo -e "│ 'cppcheck', 'wget'                    │"
-			echo -e "│                                       │"
-			echo -e "│ Como usar ex: run arquivo.c           │"
-			echo -e "│                                       │"
-			echo -e "└───────────────────────────────────────┘"
-			echo -e "\n"
+			help_f
 		;;
 		-c | --create-file)
-			# novo arquivo de estrutura minima para C, usando entrata de texto
-			read -p ' New file: ' nome
-			echo -e "\n"
-			wget https://gist.githubusercontent.com/DIEGOHORVATTI/f1272263f3db0118f2d5d5bb3df0de8d/raw/8346a43c9b3c75a463ad6b500d89699eca76e9b8/min.c -O $nome.c
-			code $nome.c
-			clear
+			creat_file_f
 		;;
 		-g | --gitgnore)
-			wget https://gist.githubusercontent.com/DIEGOHORVATTI/30462a7868ae1df718a6c32c505a9ea3/raw/77d407af7e041727e6025f30531683aaab893a7d/.gitignore
-			clear
-			echo -e "\n"
-			echo -e "┌───────────────────────────────────────┐"
-			echo -e "│                                       │"
-			echo -e "│        Create file:  .gitgnore       │"
-			echo -e "│                                       │"
-			echo -e "└───────────────────────────────────────┘"
-			echo -e "\n"
+		gitgnore_f
 		;;
 		*)
 			# verificar que a flag é uma pasta e printa seu conteúdo
@@ -68,23 +83,35 @@ for i in "$@"; do
 					clear && 
 					cppcheck ./$i && 
 					echo -e "\n───────────────────────────────────────\n" && 
-					gcc ./$i -o /tmp/run.exe && 
+					g++ ./$1 -o /tmp/run.exe && 
 					/tmp/run.exe && 
 					echo -e "\n\n───────────────────────────────────────\n"
 				else
-				# verificar que a flag usada não é nem arquivo em diretório e printa o conteúdo
-				echo -e "\n"
-				echo -e "┌───────────────────────────────────────"
-				echo -e "│                                       "
-				echo -e "│    Arquivo ou diretório inexistente   "
-				echo -e "│                                       "
-				echo -e "│    Erro: $i"  
-				echo -e "│                                       "
-				echo -e "└───────────────────────────────────────"
-				echo -e "\n"
-				colorls --group-directories-first --almost-all
-				echo -e "\n"
+					# verificar que a flag usada não é nem arquivo em diretório e printa o conteúdo
+					echo -e "\n"
+					echo -e "┌───────────────────────────────────────"
+					echo -e "│                                       "
+					echo -e "│    Arquivo ou diretório inexistente   "
+					echo -e "│                                       "
+					echo -e "│    Erro: $i"  
+					echo -e "│                                       "
+					echo -e "└───────────────────────────────────────"
+					echo -e "\n"
+					colorls --group-directories-first --almost-all
+					echo -e "\n"
+					read -p ' Create New file?[S/s,N/n]: ' quest
+					case $quest in
+						"s" | "S")
+							creat_file_f
+						;;
+						"n" | "N")
+							exit
+						;;
+						*)
+							exit
+						;;
+					esac
 			fi
-		;;
+	;;
 	esac
 done
